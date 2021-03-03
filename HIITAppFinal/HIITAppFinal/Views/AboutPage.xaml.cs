fa9b446f7505d68673784f040debcf039404d0a0
Matthew <60328996+Mattpet26@users.noreply.github.com>
2021-03-03 15:04:19 -0800

    added some forms and styling

--- HIITAppFinal/HIITAppFinal/Views/AboutPage.xaml.cs
+++ HIITAppFinal/HIITAppFinal/Views/AboutPage.xaml.cs
@@ -11,5 +11,49 @@ namespace HIITAppFinal.Views
         {
             InitializeComponent();
         }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+        void Login_Button_Clicked(object sender, System.EventArgs e)
+        {
+            Application.Current.Properties["UserName"] = UserName.Text;
+            Application.Current.Properties["Password"] = Password.Text;
+            UserName.Text = string.Empty;
+            Password.Text = string.Empty;
+            DisplayAlert("Success", "Your information has been recorded!", "Ok");
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                LoginSection.SetValue(IsVisibleProperty, false);
+                LogoutButton.SetValue(IsVisibleProperty, true);
+                Clicky.SetValue(IsVisibleProperty, true);
+            });
+
+            if (Application.Current.Properties.ContainsKey("UserName"))
+            {
+                ViewUserName.Text = Application.Current.Properties["UserName"].ToString();
+                ViewPassword.Text = Application.Current.Properties["Password"].ToString();
+            }
+        }
+        void Logout_Clicked(object sender, System.EventArgs e)
+        {
+            if (Application.Current.Properties.ContainsKey("UserName"))
+            {
+                Application.Current.Properties.Remove("UserName");
+                Application.Current.Properties.Remove("Password");
+                ViewUserName.Text = "";
+                ViewPassword.Text = "";
+            }
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                LoginSection.SetValue(IsVisibleProperty, true);
+                LogoutButton.SetValue(IsVisibleProperty, false);
+                Clicky.SetValue(IsVisibleProperty, false);
+            });
+            DisplayAlert("Success", "You have been logged out", "Ok");
+        }
     }
 }
